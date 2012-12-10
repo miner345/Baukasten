18af04cdcfe29373074c676f11af5683c9e1a679
Matt <info@miner345.de>
2012-12-10 20:43:40 +0100

    Sidebar option in Extension.php added

--- example/extension.php
+++ example/extension.php
@@ -21,6 +21,10 @@ class ExtensionConfig {
 		public $main_php = "main.php";
 		# You need to load your additional PHP files like
 		# classes on your own.
+		
+		# Sidebar File
+		# Only useful, if you set $sidebar to true in the Options below
+		public $sidebar_file = "";
 	
 	# Extension options 
 
@@ -30,6 +34,9 @@ class ExtensionConfig {
 		# Author
 		public $author = "miner345";
 		
+		# Sidebar - Set to true to get an Sidebar
+		# Define the Sidebar file in the Additional Files parts above.
+		public $sidebar = false;
 }
 
 
