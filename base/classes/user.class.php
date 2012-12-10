cba4b1cc820b7c63e5386b7a3bb7d2047b5b430a
Munir <munir.ascieli@hotmail.de>
2012-12-10 21:23:04 +0100

    Added changePassword
    Added changePassword

--- base/classes/user.class.php
+++ base/classes/user.class.php
@@ -34,9 +34,30 @@ class User {
  * changePassword: Change the password
  */
  
-    public function changePassword($user, $email = "") {
-        
-        
+    public function changePassword($user, $oldpw, $npw, $npwrep) {
+        if(isset($oldpw) && isset($npw) && isset($npwrep)) {
+            $oldpw = md5(sha1($oldpw));
+            if($checkoldpw = $this->mysql->getMA($this->config->table_prefix."user", array(0 => 'username', 1 => 'password'), array(0 => "$user", 1 => "$oldpw"))) {
+                if($npw == $npwrep) {
+                    $npwrep = md5(sha1($npwrep));
+                    if($this->mysql->query("UPDATE `user` SET `".$npwrep."`='' WHERE (`username`='".$user."') AND (`password`='".$oldpw."')")) {
+                        return true;
+                    }
+                    else throw new Exception('ERROR in changePassword!');
+                }
+                else throw new Exception('The new Passwords don´t match!');
+            }
+            else throw new Exception('The Old password is false!');
+        }
+        else throw new Exception('You need to give all parameter!');
+    }
+    
+    public function forgotPassword($user, $email = "") {
+        if(isset($user) || isset($email)) {
+        /*
+            Later
+        */
+        }
     }
 }
 
