1eca049e02636c99eeb28f23960aaa2463e1e5a8
Matt <info@miner345.de>
2012-12-10 18:44:18 +0100

    Adding Table "user" to install.php

--- install.php
+++ install.php
@@ -14,6 +14,19 @@
  * Create Tables
  */
 
+ $success = true;
+
+$res1 = mysql_query("CREATE TABLE `".$config->table_prefix."user` (
+`id` INT( 255 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`name` VARCHAR( 100 ) NOT NULL ,
+`email` VARCHAR( 100 ) NOT NULL ,
+`password` VARCHAR( 5000 ) NOT NULL ,
+`register_ip` VARCHAR( 50 ) NOT NULL ,
+`last_ip` VARCHAR( 50 ) NOT NULL ,
+`register_timestamp` INT( 100 ) NOT NULL ,
+`last_timestamp` INT( 100 ) NOT NULL )");
+
+if(!$res1) $success = false;
 
 
 /**
